26b16c56307d7b9757400e0aca421f14be759595
Castroo99 <48692149+Castroo99@users.noreply.github.com>
2023-09-21 22:02:11 +0200

    added add two numbers problem and solution

---
+++ LeetCodeProblems/AddTwoNumbers/AddTwoNumbersSolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.AddTwoNumbers
+{
+    public class AddTwoNumbersSolution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode head = new ListNode();
+            var pointer = head;
+            int overflow = 0;
+            while (l1 != null || l2 != null)
+            {
+                overflow = (l1 is null ? 0 : l1.val) + (l2 is null ? 0 : l2.val) + overflow;
+                pointer.next = new ListNode(overflow % 10);
+                pointer = pointer.next;
+                
+                overflow = overflow / 10;
+                
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+            
+            if (overflow != 0)
+            {
+                pointer.next = new ListNode(overflow);
+            }
+            return head.next;
+        }
+    }
+}
